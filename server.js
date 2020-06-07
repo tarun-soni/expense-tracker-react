b2628d1d1278bf85f5af00ebaa2003cdd510aae7
tarun-soni <soni.tarun26@gmail.com>
2020-06-07 22:10:41 +0530

    backend init with express

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const colors = require('colors')
+
+dotenv.config({path:'./config/config.env'})
+const transactions = require('./routes/transaction')
+const app = express()
+
+app.use('/api/v1/transactions',transactions)
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT,console.log(`server  in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
