e703fb8a4e0bf335e81f027f4ac70b29507e6260
tarun-soni <soni.tarun26@gmail.com>
2020-06-09 19:18:50 +0530

    can add and delete transaction in backend

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -25,13 +25,71 @@ exports.getTransactions = async (req, res, next) => {
 //@route POST api/v1/transactions
 //@access  PUBLIC 
 exports.addTransaction = async (req, res, next) => {
-    // res.send('POST transactions');
+    try {
+        const { text, amount } = req.body;
+
+        const transaction = await Transaction.create(req.body);
+
+        return res.status(201).json(
+            {
+                success: true,
+                data: transaction
+            }
+        );
+    } catch (err) {
+
+        if (err.name === 'ValidationError') {
+            const messages = Object.values(err.errors).map(val => val.message);
+
+            return res.status(400).json(
+                {
+                    success: false,
+                    error: messages
+                })
+                ;
+        } else {
+            return res.status(500).json(
+                {
+                    success: false,
+                    error: 'Server Error'
+                }
+            );
+        }
+    }
 }
 
 
-//@desc DELETE all transactions
+//@desc DELETE all transaction  s
 //@route POST api/v1/transactions/:id
 //@access  PUBLIC 
 exports.deleteTransaction = async (req, res, next) => {
-    // res.send('DELETE transactions');
+    try {
+
+        const transaction = await Transaction.findById(req.params.id);
+
+        if (!transaction) {
+            return res.status(404).json(
+                {
+                    success: false,
+                    error: 'no Trasnsaction found'
+                }
+            );
+        }
+        await transaction.remove()
+        return res.status(200).json(
+            {
+                success: true,
+                data: {}
+
+            }
+        );
+
+    } catch (error) {
+        return res.status(500).json(
+            {
+                success: false,
+                error: 'Server Error'
+            }
+        );
+    }
 }
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const colors = require('colors')
+const morgan = require('morgan')
 const connectDB = require('./config/db');
 dotenv.config({path:'./config/config.env'})
 
@@ -8,6 +9,12 @@ dotenv.config({path:'./config/config.env'})
 connectDB()
 const transactions = require('./routes/transaction')
 const app = express()
+
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+  }
+
+app.use(express.json());
 
 app.use('/api/v1/transactions',transactions)
 
