3f260a67f2e19ec11d09d0601da3d9aca470e48f
tarun-soni <soni.tarun26@gmail.com>
2020-06-09 21:30:43 +0530

    can add trans into db

--- client/src/context/GlobalState.js
+++ client/src/context/GlobalState.js
@@ -49,15 +49,33 @@ export const GlobalProvider = ({ children }) => {
             dispatch({
                 type: 'TRANSACTION_ERROR',
                 payload: err.response.data.error
-              });
+            });
         }
 
     }
-    function addTransaction(transaction) {
-        dispatch({
-            type: 'ADD_TRANSACTION',
-            payload: transaction
-        })
+    async function addTransaction(transaction) {
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        try {
+            const res = await axios.post('/api/v1/transactions', transaction, config);
+
+            dispatch({
+                type: 'ADD_TRANSACTION',
+                payload: res.data.data
+            })
+            
+        } catch (err) {
+            dispatch({
+                type: 'TRANSACTION_ERROR',
+                payload: err.response.data.error
+            });
+        }
+
     }
     return (
         <GlobalContext.Provider value={{
